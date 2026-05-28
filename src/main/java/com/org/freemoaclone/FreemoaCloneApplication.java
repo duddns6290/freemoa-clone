402f93b38a94f54280f5a10e8e64617b3fb715b0
duddns6290 <duddns6290@naver.com>
2026-05-28 15:50:44 +0900

    base pj

---
+++ src/main/java/com/org/freemoaclone/FreemoaCloneApplication.java
@@ -0,0 +1,13 @@
+package com.org.freemoaclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreemoaCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreemoaCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/org/freemoaclone/FreemoaCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.freemoaclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreemoaCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
