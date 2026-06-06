769f87ec2ea94f1fef3c27ef13adb7cc8b4ab89a
duddns6290 <duddns6290@naver.com>
2026-06-06 18:50:27 +0900

    fix: 기본 이미지 config 수정

--- src/main/front/vite.config.js
+++ src/main/front/vite.config.js
@@ -14,6 +14,10 @@ export default defineConfig({
         target: 'http://localhost:8080',
         changeOrigin: true,
       },
+      '/base_img': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
     },
   },
 })
